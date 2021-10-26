ad8cd7df25b46491d8461e5149a797e105917e94
Edu. Flores <edu@ideastm.com>
2021-10-26 09:26:32 -0500

    add migrations for courses and lessons

---
+++ database/migrations/2021_10_26_135720_create_lessons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained();
+            $table->string('name',100);
+            $table->string('summary')->nullable();
+            $table->integer('number')->default(0);
+            $table->text('content')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Course::factory(1000)->create();
+        \App\Models\User::factory(1)->create(['email'=>'edu@demo.com']);
     }
 }
